d9ab65fb2832b36460bfd49a170d12ca48ee9213
Jabroni <ranbonagarjuna@gmail.com>
2017-11-11 21:45:55 +0530

    Add files via upload

---
+++ node.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self, initdata):
+        self.data = initdata
+        self.next = None
+
+    def get_data(self):
+        return self.data
+
+    def get_next(self):
+        return self.next
+
+    def set_data(self, newdata):
+        self.data = newdata
+
+    def set_next(self, newnext):
+        self.next = newnext
+
+
+temp = Node(93)
+print(temp.get_data())
+print(temp.get_next())
