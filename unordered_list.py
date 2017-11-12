4ea7d177a6f97df6a1b304f86601c0d2acfbf909
Jabroni <ranbonagarjuna@gmail.com>
2017-11-12 12:07:41 +0530

    Add files via upload

--- unordered_list.py
+++ unordered_list.py
@@ -25,7 +25,7 @@ class Unordered_list:
         return self.head == None
 
     def add_item(self, item):
-        temp = Node()
+        temp = Node(item)
         temp.set_next(self.head)
         self.head = temp
 
@@ -39,3 +39,45 @@ class Unordered_list:
         return count
 
     def search(self, item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.get_data() == item:
+                found = True
+            else:
+                current = current.get_next()
+
+        return found
+
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.get_data() == item:
+                found = True
+            else:
+                previous = current
+                current = current.get_next()
+
+        if previous == None:
+            self.head = current.get_next()
+        else:
+            previous.set_next(current.get_next())
+
+
+mylist = Unordered_list()
+mylist.add_item(31)
+mylist.add_item(77)
+mylist.add_item(17)
+mylist.add_item(93)
+mylist.add_item(26)
+mylist.add_item(54)
+print(mylist.size())
+print(mylist.search(93))
+print(mylist.search(100))
+mylist.add_item(100)
+print(mylist.search(100))
+print(mylist.size())
+mylist.remove(93)
+print(mylist.size())
