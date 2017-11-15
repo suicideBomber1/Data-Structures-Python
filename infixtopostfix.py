512e7054eaeedb22c6367e57d5f5dd2189391c75
Jabroni <ranbonagarjuna@gmail.com>
2017-11-15 12:12:53 +0530

    Add files via upload

--- infixtopostfix.py
+++ infixtopostfix.py
@@ -1,4 +1,5 @@
 from pythonds.basic.stack import Stack
+from Parenchecker import parenchecker
 
 
 def infix_to_postfix(k):
@@ -32,17 +33,49 @@ def infix_to_postfix(k):
     return "".join(postfix)
 
 
-
 def validate_input(expr):
     if not expr:
         return False
     expr = expr.strip()
-    
+    cp = []
+    for e in expr:
+        if e == '(' or e == ')':
+            cp.append(e)
+
+    parens = ''.join(cp)
+    if not parenchecker(parens):
+        return False
+    tokens = expr.split(" ")
+    clean_tokens = []
+    for t in tokens:
+        t = t.strip()
+        clean_tokens.append(t)
+        if t.isdigit():
+            continue
+        elif t == '+' or t == '-' or t == '/' or t == '*' or t == '^':
+            continue
+        elif t == '(' ot t == ')':
+            continue
+        else:
+            return False
+    return clean_tokens
 
 
+def read_input():
+    print("Enter the inflix expression with spaces. ")
+    infix = input("infix expression: ")
+    ip = validate_input(infix)
+    while not ip:
+        print("Not a valid expression! Please enter the correct infix input.")
+        print("Enter the correct infix expression with space.")
+        infix = input("infix expression: ")
+    return ip
 
 
-
+def main():
+    infix = read_input()
+    postfix = infix_to_postfix(infix)
+    print("Post infix expression is: ", postfix)
 
 
 # print(infix_to_postfix("A * B + C * D"))
