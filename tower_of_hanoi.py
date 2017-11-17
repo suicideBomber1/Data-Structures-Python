e8c0bcb8bc8066893e877728abd988eeaedda284
Jabroni <ranbonagarjuna@gmail.com>
2017-11-17 23:31:56 +0530

    Add files via upload

--- tower_of_hanoi.py
+++ tower_of_hanoi.py
@@ -3,3 +3,10 @@ def move_tower(height, frompole, topole, withpole):
         move_tower(height - 1, frompole, withpole, topole)
         move_disk(frompole, topole)
         move_tower(height - 1, withpole, topole, frompole)
+
+
+def move_disk(fp, tp):
+    print("Move disk from ", fp, "to ", tp)
+
+
+move_tower(5, "A", "B", "C")
